b4791759850bca4061251fb645d4ed1e890019b6
Vinson Beduya <vinsonemb.1551994@gmail.com>
2022-03-09 16:45:26 +1300

    added tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { unmountComponentAtNode } from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<App />, div);
+  unmountComponentAtNode(div);
+});
+
+describe('Increment', () => {
+  it('knows that clicking the increment button twice is 2', () => {
+    render(<App />);
+    const incrementCountButton = screen.getByText("Increment count");
+
+    // Click twice
+    fireEvent.click(incrementCountButton);
+    fireEvent.click(incrementCountButton);
+    
+    const actual = screen.getByText(/Count: \d/);
+    const expected = "Count: 2";
+    expect(actual).toHaveTextContent(expected);
+  })
 });
